74dadc7a196979f5aff284a216c2991f3540c256
Abdoulaye Ly <abdoulayely148@gmail.com>
2025-11-10 02:30:02 +0000

    deploy6

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Attendre que la base de données soit prête
 echo "Waiting for database to be ready..."
-while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME; do
+while ! pg_isready -h $DB_HOST -p $DB_PORT -U "$DB_USERNAME"; do
   echo "Database is unavailable - sleeping"
   sleep 2
 done
